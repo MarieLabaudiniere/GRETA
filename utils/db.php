fe511a94da3602774122fe1daeb02ad78132cb48
MarieLabaudiniere <marie.labaudiniere@greta-paysdelaloire.fr>
2022-04-19 14:46:51 +0200

    changement pwd

--- utils/db.php
+++ utils/db.php
@@ -3,14 +3,14 @@
 $server = "localhost";
 $db = "greta";
 $user = "web2";
-$passwd = "12345";
+$secret = "12345";
 // Fin de la déclaration des paramètres
 
 // Cette partie est générique à l'ensemble de vos projets utilisant une base de données.
 $dsn = "mysql:host=$server;dbname=$db";
 
 try {
-    $pdo = new PDO($dsn, $user, $passwd);
+    $pdo = new PDO($dsn, $user, $secret);
     // set the PDO error mode to exception
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   } catch(PDOException $e) {
